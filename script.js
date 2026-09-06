33627318f4469380d72cc986f6c2e3839ddd9e0a
yusup604 <yusupsupriadi604@gmail.com>
2026-09-06 08:54:57 +0700

    Update GOOGLE_SCRIPT_URL to new Google Apps Script URL

--- script.js
+++ script.js
@@ -290,7 +290,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // =========================================================================
 // 1. URL WEB APP GOOGLE APPS SCRIPT ANDA (SUDAH TERSAMBUNG REKAP ABSENSI)
 // =========================================================================
-const GOOGLE_SCRIPT_URL = "https://google.com";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyQ_YloF6OtJlqydibxLghluIRRyaATltZmbQyK-qsDblejaLgIb65yBSjEvaLOdGesSA/exec";
 
 document.addEventListener("DOMContentLoaded", function () {
   
